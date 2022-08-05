2da9fc5850b50c5c83382d6ee235c7fde49b46a5
Gessivam Júnior <juniorgessivam@gmail.com>
2022-08-04 21:18:01 -0300

    feat: add CountriesService

--- src/countries/countries.service.ts
+++ src/countries/countries.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CovidCountryDto } from './dto/covid-country.dto';
+import fetch from 'node-fetch';
 
 @Injectable()
-export class CountriesService {}
+export class CountriesService {
+  async fetchCountries(countryOne: string, countryTwo: string) {
+    try {
+      const response = await fetch(
+        `https://disease.sh/v3/covid-19/countries/${countryOne}%2C%20${countryTwo}?yesterday=yesterday`,
+      );
+      const countriesData: CovidCountryDto[] = await response.json();
+      return countriesData;
+    } catch (error) {
+      console.error('Cannot fetch countries from Disease API');
+    }
+  }
+}
