cfc94de61b8990136e5967b7d011950a6cae484f
Gessivam Junior <juniorgessivam@gmail.com>
2022-08-11 19:40:20 -0300

    fix: CronExpression to every_day_at_10pm

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,16 +1,14 @@
-import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { CountriesService } from 'src/countries/countries.service';
 import { GofileService } from 'src/gofile/gofile.service';
 
-@Injectable()
 export class JobsService {
   constructor(
     private readonly countriesService: CountriesService,
     private readonly gofileService: GofileService,
   ) {}
 
-  @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_DAY_AT_10PM)
   async handleCron() {
     const westernData = await this.countriesService.fetchCountries(
       'usa',
